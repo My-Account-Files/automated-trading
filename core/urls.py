90c2cf6c2204f3bff09d3def526e9229ad7f77a6
tayyab2914 <tayyab1234rehman@gmail.com>
2024-01-28 21:26:15 +0500

    Linked css files

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', index, name="index"),
     path('signin/', signin, name="signin"),
     path('signup/', signup, name="signup"),
-    path('verify_otp', verify_otp, name="verify_otp"),
-    path('dashboard', dashboard, name="dashboard"),
+    path('verify_otp/', verify_otp, name="verify_otp"),
+    path('dashboard/', dashboard, name="dashboard"),
 ]
