cb77fae9523927d54da9560334b681ff17535a55
tayyab2914 <tayyab1234rehman@gmail.com>
2024-01-31 21:41:38 +0500

    Added Database and downgrade Django

--- Trading/settings.py
+++ Trading/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'Trading.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE":"django.db.backends.postgresql",
+        "NAME":"railway",
+        "USER":"postgres",
+        "PASSWORD":"b-3BDegDe5BgAB2*34*F222B44-Bab22",
+        "HOST":"roundhouse.proxy.rlwy.net",
+        "PORT":"36744"
     }
 }
 
