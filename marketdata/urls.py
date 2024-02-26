d1fbedadc225a04e7102f6b3c1b447d4e482652a
tayyab2914 <tayyab1234rehman@gmail.com>
2024-02-26 13:40:43 +0500

    Added market_data_form

--- marketdata/urls.py
+++ marketdata/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('market_data/', market_data, name="market_data"),
+    path('market_data_form/', market_data_form, name="market_data_form"),
 ]
--- marketdata/views.py
+++ marketdata/views.py
@@ -10,7 +10,10 @@ from .models import *
 def market_data(request):
     return render(request, 'pages/market-data.html')
 
+def market_data_form(request):
+    return render(request, 'pages/market_data_form.html')
 
 def alerts(request):
     return render(request, 'pages/comingsoon.html')
+
     
